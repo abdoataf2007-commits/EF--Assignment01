9e5ed4b829c85e18e7568246092c14587cc34db5
abdoataf2007-commits <abdoataf2007@gmail.com>
2026-09-16 01:18:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using ReadMoreBooks.Data;
+
+namespace ReadMoreBooks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using BookStoreContext context = new BookStoreContext();
+
+            context.Database.EnsureCreated();
+
+            Console.WriteLine("Database created successfully!");
+
+            Console.ReadKey();
+        }
+    }
+}
